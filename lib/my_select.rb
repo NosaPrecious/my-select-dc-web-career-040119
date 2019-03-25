f13099d9a7576a6f7688e62b55b3cfbfe1fe63e0
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-25 18:28:26 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+      count= 0
+      len= collection.length
+      n_arr= []
+    if block_given?
+      while count < len
+        if yield collection[count]
+          n_arr << collection[count]
+        end
+        count+= 1
+      end
+   return n_arr
+  else
+    return collection
+  end
 end
